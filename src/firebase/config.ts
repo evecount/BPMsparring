f5b18db53205ac0e11448f6dfb57b955185274f7
Gwendalynn Lim Wan Ting <gwenlynn.lim@gmail.com>
2025-11-23 20:48:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3011518785-6cca3",
+  "appId": "1:445695397558:web:0de1666dcc221f0d11a017",
+  "apiKey": "AIzaSyAV4jgFBt-88y766oISXPXRWQn5LTVPPEk",
+  "authDomain": "studio-3011518785-6cca3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "445695397558"
+};
